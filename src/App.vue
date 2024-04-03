822c0a6f1b1f291be27911f097c2964382898fce
Yukai Huang <yukaihuangtw@gmail.com>
2024-04-03 23:05:54 +0800

    feat: color block

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import Toolbar from './components/Toolbar.vue'
 import HealthBar from './components/HealthBar.vue'
+import ColorBlock from './components/ColorBlock.vue'
 import { useGlobalGameState } from './gameState'
 
 const state = useGlobalGameState()
@@ -11,6 +12,11 @@ const stateString = JSON.stringify(state, null, 2)
   <div class="w-full h-full py-2 px-2">
     <toolbar />
     <health-bar />
+
+    <div class="flex gap-2">
+      <color-block :color="state.randomColor.value" />
+      <color-block :color="state.userColor.value" />
+    </div>
   </div>
 </template>
 
---
+++ src/components/ColorBlock.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="w-full aspect-square max-w-[50%] rounded-lg" :style="{ backgroundColor: colorStr }"></div>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+import { hsvToRgb } from '../utils'
+
+const props = defineProps({
+  color: {
+    type: Object,
+    required: true
+  }
+})
+
+const { h, s, v } = props.color
+const rgbColor = hsvToRgb(h, s, v)
+const colorStr = `rgb(${rgbColor.r}, ${rgbColor.g}, ${rgbColor.b})`
+</script>
