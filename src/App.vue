b676672e036c629dea0a3c2d2454873e2395bad4
Yukai Huang <yukaihuangtw@gmail.com>
2024-04-03 23:24:59 +0800

    feat: connect actions

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <script setup>
+import { ref } from 'vue'
 import Toolbar from './components/Toolbar.vue'
 import HealthBar from './components/HealthBar.vue'
 import ColorBlock from './components/ColorBlock.vue'
+
 import { useGlobalGameState } from './gameState'
 
 const state = useGlobalGameState()
-const stateString = JSON.stringify(state, null, 2)
+const randomColor = state.randomColor
+const userColor = state.userColor
+
+const userH = ref(userColor.value.h)
+const userS = ref(userColor.value.s)
+const userV = ref(userColor.value.v)
+
+const submit = () => {
+  state.updateUserColor(userH.value, userS.value, userV.value)
+  state.checkGuess()
+}
 </script>
 
 <template>
@@ -14,8 +26,34 @@ const stateString = JSON.stringify(state, null, 2)
     <health-bar />
 
     <div class="flex gap-2">
-      <color-block :color="state.randomColor.value" />
-      <color-block :color="state.userColor.value" />
+      <color-block :color="randomColor" />
+      <color-block :color="userColor" />
+    </div>
+
+
+    <!-- Sliders and Confirm Button -->
+    <div class="flex flex-col space-y-4">
+      <!-- Hue Slider -->
+      <div class="flex items-center">
+        <span class="text-orange-300 mr-2">H:</span>
+        <input type="range" min="0" max="360" v-model="userH" class="slider hue-slider">
+        <span class="bg-gray-700 text-white px-4 py-2 rounded-lg ml-2">{{ userH }}</span>
+      </div>
+      <!-- Saturation Slider -->
+      <div class="flex items-center">
+        <span class="text-orange-300 mr-2">S:</span>
+        <input type="range" min="0" max="100" v-model="userS" class="slider">
+        <span class="bg-gray-700 text-white px-4 py-2 rounded-lg ml-2">{{ userS }}</span>
+      </div>
+      <!-- Value Slider -->
+      <div class="flex items-center">
+        <span class="text-orange-300 mr-2">V:</span>
+        <input type="range" min="0" max="100" v-model="userV" class="slider">
+        <span class="bg-gray-700 text-white px-4 py-2 rounded-lg ml-2">{{ userV }}</span>
+      </div>
+
+      <!-- Confirm Button -->
+      <button class="bg-pink-600 px-4 py-2 rounded-lg text-white w-full button-3d" @click="submit">確定</button>
     </div>
   </div>
 </template>
--- src/components/HealthBar.vue
+++ src/components/HealthBar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex space-x-2">
-    <ph-heart :size="20" weight="fill" class="text-pink-600" v-for="life in state.lives" />
+    <ph-heart :size="20" weight="fill" class="text-pink-600" v-for="life in lives" />
   </div>
 </template>
 
@@ -9,5 +9,6 @@ import { PhHeart } from "@phosphor-icons/vue"
 import { useGlobalGameState } from '../gameState'
 
 const state = useGlobalGameState()
+const lives = state.lives
 </script>
 
