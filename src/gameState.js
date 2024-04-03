e79b7a3dce3e619cedb77a67b4e3b403b3e83ed1
Alan Chou <alan10332000@gmail.com>
2024-04-04 01:43:00 +0800

    chore: lint code

--- src/gameState.js
+++ src/gameState.js
@@ -1,102 +1,100 @@
 import { createGlobalState } from '@vueuse/core'
 import { ref, reactive } from 'vue'
 
-export const useGlobalGameState = createGlobalState(
-  () => {
-    // State
-    const currentRound = ref(1)
-    const maxLife = ref(5)
-    const lives = ref(maxLife.value)
-    const precision = ref(10)
-    const mode = ref("Color") // default to "Color", can also be "B/W"
-    const randomColor = reactive({
-      h: Math.floor(Math.random() * 360),
-      s: Math.floor(Math.random() * 100),
-      v: Math.floor(Math.random() * 100),
+export const useGlobalGameState = createGlobalState(() => {
+  // State
+  const currentRound = ref(1)
+  const maxLife = ref(5)
+  const lives = ref(maxLife.value)
+  const precision = ref(10)
+  const mode = ref('Color') // default to "Color", can also be "B/W"
+  const randomColor = reactive({
+    h: Math.floor(Math.random() * 360),
+    s: Math.floor(Math.random() * 100),
+    v: Math.floor(Math.random() * 100),
+  })
+  const userColor = reactive({ h: 0, s: 0, v: 0 }) // default to zero, user has to change
+  const score = ref(0)
+  const history = ref([])
+
+  // Getters
+  // Add any computed property getters here if required
+
+  // Actions
+  function startNewRound() {
+    // Increment the round, reset the lives and userColor
+    currentRound.value++
+    lives.value = maxLife.value
+
+    // Generate new randomColor
+    randomColor.h = Math.floor(Math.random() * 360)
+    randomColor.s = Math.floor(Math.random() * 100)
+    randomColor.v = Math.floor(Math.random() * 100)
+  }
+
+  function recordRound(wasSuccess) {
+    // Record the round history
+    history.value.push({
+      round: currentRound.value,
+      guessedColor: userColor,
+      actualColor: randomColor,
+      wasSuccess: wasSuccess,
     })
-    const userColor = reactive({ h: 0, s: 0, v: 0 }) // default to zero, user has to change
-    const score = ref(0)
-    const history = ref([])
 
-    // Getters
-    // Add any computed property getters here if required
-
-    // Actions
-    function startNewRound() {
-      // Increment the round, reset the lives and userColor
-      currentRound.value++
-      lives.value = maxLife.value
-
-      // Generate new randomColor
-      randomColor.h = Math.floor(Math.random() * 360)
-      randomColor.s = Math.floor(Math.random() * 100)
-      randomColor.v = Math.floor(Math.random() * 100)
+    // Update the score
+    if (wasSuccess) {
+      score.value++
+    } else {
+      lives.value-- // reduce life if the guess was incorrect
     }
 
-    function recordRound(wasSuccess) {
-      // Record the round history
-      history.value.push({
-        round: currentRound.value,
-        guessedColor: userColor,
-        actualColor: randomColor,
-        wasSuccess: wasSuccess,
-      })
-
-      // Update the score
-      if (wasSuccess) {
-        score.value++
-      } else {
-        lives.value-- // reduce life if the guess was incorrect
-      }
-
-      // Check if we need to start a new round
-      if (wasSuccess || lives.value === 0) {
-        startNewRound() // user guessed correctly or all lives were used, so start a new round
-      }
+    // Check if we need to start a new round
+    if (wasSuccess || lives.value === 0) {
+      startNewRound() // user guessed correctly or all lives were used, so start a new round
     }
+  }
 
-    // Action to update user's chosen color
-     function updateUserColor(h, s, v) {
-        userColor.h = h
-        userColor.s = s
-        userColor.v = v
-     }
+  // Action to update user's chosen color
+  function updateUserColor(h, s, v) {
+    userColor.h = h
+    userColor.s = s
+    userColor.v = v
+  }
 
-     // Action to change game mode
-     function updateMode(newMode) {
-        mode.value = newMode
-     }
+  // Action to change game mode
+  function updateMode(newMode) {
+    mode.value = newMode
+  }
 
-     // Action to change precision value
-     function updatePrecision(newPrecision) {
-        precision.value = newPrecision
-     }
+  // Action to change precision value
+  function updatePrecision(newPrecision) {
+    precision.value = newPrecision
+  }
 
-     // Action to check if user's guess is correct or not
-     function checkGuess() {
-        const hIsGood = Math.abs(randomColor.h - userColor.h) <= precision.value;
-        const sIsGood = Math.abs(randomColor.s - userColor.s) <= precision.value;
-        const vIsGood = Math.abs(randomColor.v - userColor.v) <= precision.value;
+  // Action to check if user's guess is correct or not
+  function checkGuess() {
+    const hIsGood = Math.abs(randomColor.h - userColor.h) <= precision.value
+    const sIsGood = Math.abs(randomColor.s - userColor.s) <= precision.value
+    const vIsGood = Math.abs(randomColor.v - userColor.v) <= precision.value
 
-        recordRound(hIsGood && sIsGood && vIsGood);
-     }
+    recordRound(hIsGood && sIsGood && vIsGood)
+  }
 
-    return {
-      currentRound,
-      maxLife,
-      lives,
-      precision,
-      mode,
-      randomColor,
-      userColor,
-      score,
-      history,
-      startNewRound,
-      recordRound,
-      updateUserColor,
-      updateMode,
-      updatePrecision,
-      checkGuess,
-    }
+  return {
+    currentRound,
+    maxLife,
+    lives,
+    precision,
+    mode,
+    randomColor,
+    userColor,
+    score,
+    history,
+    startNewRound,
+    recordRound,
+    updateUserColor,
+    updateMode,
+    updatePrecision,
+    checkGuess,
+  }
 })
-
--- src/utils.js
+++ src/utils.js
@@ -1,61 +1,61 @@
-export function hsvToRgb (h, s, v) {
-  let r, g, b;
+export function hsvToRgb(h, s, v) {
+  let r, g, b
 
-  let i;
-  let f, p, q, t;
+  let i
+  let f, p, q, t
 
-  s /= 100;
-  v /= 100;
+  s /= 100
+  v /= 100
 
-  h /= 60; // sectors (0 to 6)
-  i = Math.floor(h);
-  f = h - i; // fractional part of h
+  h /= 60 // sectors (0 to 6)
+  i = Math.floor(h)
+  f = h - i // fractional part of h
 
-  p = v * (1 - s);
-  q = v * (1 - s * f);
-  t = v * (1 - s * (1 - f));
+  p = v * (1 - s)
+  q = v * (1 - s * f)
+  t = v * (1 - s * (1 - f))
 
   switch (i) {
     case 0:
-      r = v;
-      g = t;
-      b = p;
-      break;
+      r = v
+      g = t
+      b = p
+      break
 
     case 1:
-      r = q;
-      g = v;
-      b = p;
-      break;
+      r = q
+      g = v
+      b = p
+      break
 
     case 2:
-      r = p;
-      g = v;
-      b = t;
-      break;
+      r = p
+      g = v
+      b = t
+      break
 
     case 3:
-      r = p;
-      g = q;
-      b = v;
-      break;
+      r = p
+      g = q
+      b = v
+      break
 
     case 4:
-      r = t;
-      g = p;
-      b = v;
-      break;
+      r = t
+      g = p
+      b = v
+      break
 
     default: // case 5:
-      r = v;
-      g = p;
-      b = q;
-      break;
+      r = v
+      g = p
+      b = q
+      break
   }
 
-  r = Math.round(r * 255);
-  g = Math.round(g * 255);
-  b = Math.round(b * 255);
+  r = Math.round(r * 255)
+  g = Math.round(g * 255)
+  b = Math.round(b * 255)
 
-  return { r, g, b };
+  return { r, g, b }
 }
