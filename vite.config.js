c3d2ebb864d22c354bd2a82f348e5214d0939a36
Yukai Huang <yukaihuangtw@gmail.com>
2024-04-04 10:59:57 +0800

    feat: pwa things

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -20,5 +21,26 @@ export default defineConfig({
     }),
     ,
     Components(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'HSV Hunter',
+        short_name: 'HSV Hunter',
+        description: 'Use sliders, guess the color, and improve your color regconition skill!',
+        theme_color: '#222222',
+        // icons: [
+        //   {
+        //     src: 'pwa-192x192.png',
+        //     sizes: '192x192',
+        //     type: 'image/png'
+        //   },
+        //   {
+        //     src: 'pwa-512x512.png',
+        //     sizes: '512x512',
+        //     type: 'image/png'
+        //   },
+        // ]
+      },
+    }),
   ],
 })
