d215d97b594e542d9e3786cc53358fdc98da39da
Yukai Huang <yukaihuangtw@gmail.com>
2024-04-05 19:05:30 +0800

    style: lint

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,9 +1,9 @@
 <template>
-    <div :class="className" @click="onClose">
-        <div class="w-[90%] max-w-xs bg-[#1f2937] rounded-md p-4 z-50" @click.stop>
-            <slot/>
-        </div>
+  <div :class="className" @click="onClose">
+    <div class="z-50 w-[90%] max-w-xs rounded-md bg-[#1f2937] p-4" @click.stop>
+      <slot />
     </div>
+  </div>
 </template>
 
 <script setup>
@@ -11,11 +11,21 @@ import { twMerge } from 'tailwind-merge'
 
 const props = defineProps({
   isOpen: Boolean,
-  onClose: Function,
-  modalClass: String
-});
+  onClose: {
+    type: Function,
+    default: () => {},
+  },
+  modalClass: {
+    type: String,
+    default: '',
+  },
+})
 
 const className = computed(() => {
-  return twMerge('fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-50 z-50', props.modalClass, !props.isOpen && 'hidden')
+  return twMerge(
+    'fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-50 z-50',
+    props.modalClass,
+    !props.isOpen && 'hidden'
+  )
 })
 </script>
